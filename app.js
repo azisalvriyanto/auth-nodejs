c57a26efa458cb5859faf1706e062edf7aefb291
azisalvriyanto <aalvriyanto@gmail.com>
2023-05-05 10:45:49 +0700

    (init) app: init file running with 1337 port

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+require('dotenv/config')
+
+const app = express()
+const PORT = process.env.PORT
+
+// Middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({
+        status: true,
+        message: 'Made with love by Alvriyanto Azis',
+        data: null
+    })
+})
+
+
+// Start listen 
+app.listen(PORT, () => {
+    console.log(`Server running in ${PORT}`)
+})
